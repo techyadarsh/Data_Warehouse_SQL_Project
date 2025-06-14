06e04576d281e1586ceadbb35567301d9faab42c
Adarsh Wankhade <140607527+techyadarsh@users.noreply.github.com>
2025-06-14 16:47:36 +0530

    Update create_bronze_tables.sql

--- Scripts/bronze/create_bronze_tables.sql
+++ Scripts/bronze/create_bronze_tables.sql
@@ -23,7 +23,6 @@ use DataWarhouse;
 
 IF OBJECT_ID ('bronze.crm_cust_info','U') IS NOT NULL
 	DROP TABLE bronze.crm_cust_info;
-
 CREATE TABLE bronze.crm_cust_info(
 	cst_id			INT,
 	cst_key 		NVARCHAR(50),
@@ -36,7 +35,6 @@ CREATE TABLE bronze.crm_cust_info(
 
 IF OBJECT_ID ('bronze.crm_prd_info','U') IS NOT NULL
 	DROP TABLE bronze.crm_prd_info;
-
 CREATE TABLE bronze.crm_prd_info(
 	prd_id			INT,
 	prd_key			NVARCHAR(50),
@@ -50,7 +48,6 @@ CREATE TABLE bronze.crm_prd_info(
 
 IF OBJECT_ID ('bronze.crm_sales_details','U') IS NOT NULL
 	DROP TABLE bronze.crm_sales_details;
-
 CREATE TABLE bronze.crm_sales_details(
 	sls_ord_num		NVARCHAR(50),
 	sls_prd_key		NVARCHAR(50),
@@ -66,7 +63,6 @@ CREATE TABLE bronze.crm_sales_details(
 
 IF OBJECT_ID ('bronze.erp_cust_az12','U') IS NOT NULL
 	DROP TABLE bronze.erp_cust_az12;
-
 CREATE TABLE bronze.erp_cust_az12(
 	CID		NVARCHAR(50),
 	BDATE		DATE,
@@ -76,7 +72,6 @@ CREATE TABLE bronze.erp_cust_az12(
 
 IF OBJECT_ID ('bronze.erp_loc_a101','U') IS NOT NULL
 	DROP TABLE bronze.erp_loc_a101;
-
 CREATE TABLE bronze.erp_loc_a101(
 	CID		NVARCHAR(50),
 	CNTRY		NVARCHAR(50)
@@ -85,7 +80,6 @@ CREATE TABLE bronze.erp_loc_a101(
 
 IF OBJECT_ID ('bronze.erp_px_cat_g1v2','U') IS NOT NULL
 	DROP TABLE bronze.erp_px_cat_g1v2;
-
 CREATE TABLE bronze.erp_px_cat_g1v2(
 	ID			NVARCHAR(50),
 	CAT			NVARCHAR(50),
